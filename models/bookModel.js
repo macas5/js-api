d573f547a05ac05dfb19a8b5da96b5d5ca3bfe41
macas5 <mkalinauskas1993@gmail.com>
2022-09-09 12:59:18 +0300

    fix schema naming

--- models/bookModel.js
+++ models/bookModel.js
@@ -19,7 +19,7 @@ const bookSchema = new mongoose.Schema ({
         required: true
     },
     description: String,
-    condition: String
+    bookCondition: String
 }, {timestamps: true});
 
 export default mongoose.model('book', bookSchema);
