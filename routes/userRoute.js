5f5dcb2a874987f6598b70ee965ba6d23d8e783e
macas5 <mkalinauskas1993@gmail.com>
2022-09-12 10:38:31 +0300

    fix middleware naming in routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { verifySessionTokenAdmin, verifySessionTokenUser } from "../authCheck/authCheck.js";
 import { getAllUsers, getUserById, deleteUserById, updateUser } from "../controller/userController.js";
-import validateSession from "../utils/sessionValidation.js";
+import sessionValidation from "../utils/sessionValidation.js";
 
 const router = express.Router();
 
-router.get('/get', validateSession, verifySessionTokenAdmin, getAllUsers);
+router.get('/get', sessionValidation, verifySessionTokenAdmin, getAllUsers);
 
-router.get('/get/:id', validateSession, verifySessionTokenUser, getUserById);
+router.get('/get/:id', sessionValidation, verifySessionTokenUser, getUserById);
 
-router.delete('/delete/:id', validateSession, verifySessionTokenUser, deleteUserById);
+router.delete('/delete/:id', sessionValidation, verifySessionTokenUser, deleteUserById);
 
-router.put('/update/:id', validateSession, verifySessionTokenUser, updateUser);
+router.put('/update/:id', sessionValidation, verifySessionTokenUser, updateUser);
 
 export default router;
